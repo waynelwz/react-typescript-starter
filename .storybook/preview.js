9ae2f589e0f565eb550e55907fcb523eccecd741
waynelwz <wytheme.go@gmail.com>
2022-03-16 17:45:41 +0800

    Add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,37 @@
+
+import React from 'react'
+import { Client as Styletron } from 'styletron-engine-atomic'
+import { Provider as StyletronProvider } from 'styletron-react'
+import { LightTheme, BaseProvider, DarkTheme } from 'baseui'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { useCurrentThemeType } from '@/hooks/useCurrentThemeType'
+// import { configure, addDecorator } from "@storybook/react"
+
+const engine = new Styletron()
+const queryClient = new QueryClient()
+
+const ThemeDecorator = storyFn => {
+    const themeType = useCurrentThemeType()
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <StyletronProvider value={engine}>
+                <BaseProvider theme={themeType === 'dark' ? DarkTheme : LightTheme}>
+                    {storyFn()}                        
+                </BaseProvider>
+            </StyletronProvider>
+        </QueryClientProvider>
+    )
+}
+
+export const decorators  = [ThemeDecorator]
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
