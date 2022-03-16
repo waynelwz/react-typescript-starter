d3a78268d246c309816aa0c72ac5eba9c1ba5e99
waynelwz <wytheme.go@gmail.com>
2022-03-16 18:53:57 +0800

    Test ts mock props

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import { ComponentStory, ComponentMeta } from '@storybook/react'
+// import { ComponentStory, ComponentMeta, Story } from '@storybook/react'
+import { Story, Meta } from '@storybook/react/types-6-0'
 
-import { Button } from 'baseui/button'
+import { createMock } from 'ts-auto-mock'
+
+import { Button, ButtonProps } from 'baseui/button'
 
 // import { Button } from './Button'
 
@@ -10,22 +13,22 @@ export default {
     title: 'Example/Button',
     component: Button,
     // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-    argTypes: {
-        backgroundColor: { control: 'color' },
-    },
-} as ComponentMeta<typeof Button>
+    // argTypes: {},
+} as Meta
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 /* eslint-disable react/jsx-props-no-spreading */
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
+const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
 export const Primary = Template.bind({})
+Primary.args = createMock<ButtonProps>()
+
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-    // primary: true,
-    // label: 'Button',
-    // displayName: '123',
-}
+// Primary.args = {
+//     // primary: true,
+//     // label: 'Button',
+//     // displayName: '123',
+// }
 
 // export const Secondary = Template.bind({})
 // Secondary.args = {
